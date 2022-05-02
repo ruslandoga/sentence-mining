754760885593f0fc96c99f1a65a25673110655c1
ruslandoga <67764432+ruslandoga@users.noreply.github.com>
2022-05-02 16:15:56 +0300

    reduce sqlite3 conn cache size

--- config/runtime.exs
+++ config/runtime.exs
@@ -30,7 +30,8 @@ if config_env() == :prod do
     # https://litestream.io/tips/#disable-autocheckpoints-for-high-write-load-servers
     wal_auto_check_point: 0,
     # https://litestream.io/tips/#busy-timeout
-    busy_timeout: 5000
+    busy_timeout: 5000,
+    cache_size: -2000
 
   port = String.to_integer(System.get_env("PORT") || "4000")
 
