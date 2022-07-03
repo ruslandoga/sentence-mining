ad345ba94b2f1a321aaef1d55f0c5cc6a6420ed6
ruslandoga <67764432+ruslandoga@users.noreply.github.com>
2022-07-03 16:44:17 +0300

    fix migrations

--- priv/repo/migrations/20220508111755_add_longman.exs
+++ priv/repo/migrations/20220508111755_add_longman.exs
@@ -2,7 +2,7 @@ defmodule M.Repo.Migrations.AddLongman do
   use Ecto.Migration
 
   def change do
-    create table(:longman_words, primary_key: false) do
+    create_if_not_exists table(:longman_words, primary_key: false) do
       add :user_id, :integer, null: false, primary_key: true
       add :word, :text, null: false, primary_key: true
       add :pronunciation, :text, null: false, primary_key: true
