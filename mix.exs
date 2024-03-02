0b05335ce80fbd3138b0cb7accf3424cd17d2a10
ruslandoga <67764432+ruslandoga@users.noreply.github.com>
2024-03-02 18:54:55 +0800

    require elixir 1.16

--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule M.MixProject do
     [
       app: :m,
       version: "0.1.0",
-      elixir: "~> 1.15",
+      elixir: "~> 1.16",
       elixirc_paths: elixirc_paths(Mix.env()),
       start_permanent: Mix.env() == :prod,
       aliases: aliases(),
